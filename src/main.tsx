8b6bdfaeae5e164334f75b92363381d426520b07
githubWex <github@we-x.io>
2025-02-21 16:46:35 -0600

    Refactor application structure: remove App component, add NotFound component, and implement AppWrapper for routing and loading state

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,56 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import App from './App.tsx';
-import './index.css';
+import { useEffect, useState } from "react";
 
-createRoot(document.getElementById('root')!).render(
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { LoadingScreen } from "./components/LoadingScreen";
+import { ProfileSelector } from "./components/ProfileSelector";
+import { AboutSection } from "./components/about/AboutSection";
+import { ProjectsSection } from "./components/projects/ProjectsSection";
+import { ContactSection } from "./components/ContactSection";
+import {
+  BrowserRouter as Router,
+  Navigate,
+  Route,
+  Routes,
+} from "react-router-dom";
+import { Navbar } from "./components/Navbar";
+import { NotFound } from "./components/NotFound";
+
+import "./index.css";
+
+const AppWrapper = () => {
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIsLoading(false);
+    }, 2500);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  if (isLoading) {
+    return <LoadingScreen />;
+  }
+
+  return (
+    <>
+      <Router>
+        <Routes>
+          <Route path="/" element={<ProfileSelector />} />
+          <Route path="/about" element={<AboutSection />} />
+          <Route path="/projects" element={<ProjectsSection />} />
+          <Route path="/contact" element={<ContactSection />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Navbar />
+      </Router>
+    </>
+  );
+};
+
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
+    <AppWrapper />
+  </StrictMode>,
 );
